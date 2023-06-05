44a496dd98e67094d6965de6726be4a4ea79691d
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2023-06-05 01:37:10 -0400

    Added initial sql-setup file

---
+++ db-setup.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Profile (
+    email varchar(255) NOT NULL,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    salt varchar(255) NOT NULL,
+    hash varchar(255) NOT NULL,
+    PRIMARY KEY (email),
+);
+
+CREATE TABLE PoolSettings (
+    id varchar(255) NOT NULL,
+    min_buy_in float NOT NULL,
+    max_buy_in float NOT NULL,
+    denominations varchar(255) NOT NULL,
+    has_password boolean NOT NULL,
+    salt varchar(255),
+    hash varchar(255),
+    PRIMARY KEY (id),
+);
+
+CREATE TABLE Device (
+    id varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Pool (
+    id varchar(255) NOT NULL,
+    device_id varchar(255) NOT NULL,
+    pool_name name(255) NOT NULL,
+    date_created varchar(255) NOT NULL,
+    settings_id varchar(255) NOT NULL,
+    admin_id varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (device_id) REFERENCES Device(id),
+    FOREIGN KEY (settings_id) REFERENCES PoolSettings(id),
+    FOREIGN KEY (admin_id) REFERENCES Profile(email)
+);
+
+CREATE TABLE Transaction (
+    id varchar(255) NOT NULL,
+    pool_id varchar(255) NOT NULL,
+    profile_id varchar(255) NOT NULL,
+    date varchar(255) NOT NULL,
+    type varchar(255) NOT NULL,
+    amount float NOT NULL,
+    status varchar(255) NOT NULL
+    PRIMARY KEY (id),
+    FOREIGN KEY (pool_id) REFERENCES Pool(id),
+    FOREIGN KEY (profile_id) REFERENCES Profile(email)
+);
