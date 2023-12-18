50099220cdeb4a1cff7a85dabeea05e48a421317
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2023-12-17 22:08:28 -0700

    fixed list padding issue

--- src/app/components/common/loading-spinner/loading-spinner.component.ts
+++ src/app/components/common/loading-spinner/loading-spinner.component.ts
@@ -1,35 +1,10 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { API_TIMEOUT_CONSTRAINT } from '@constants';
-import { ApiService } from 'src/app/services/api/api.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
   templateUrl: './loading-spinner.component.html',
   styleUrls: ['./loading-spinner.component.scss'],
 })
-export class LoadingSpinnerComponent implements OnInit {
-  @Input() isNetworkRequest: boolean = true;
-  @Output() requestRetry: EventEmitter<boolean> = new EventEmitter<boolean>();
-
-  public timeoutTriggered: boolean = false;
-
+export class LoadingSpinnerComponent {
   constructor() {}
-
-  ngOnInit(): void {
-    if (this.isNetworkRequest) {
-      this.triggerTimeoutCheck();
-    }
-  }
-
-  triggerTimeoutCheck() {
-    this.timeoutTriggered = false;
-    setTimeout(() => {
-      this.timeoutTriggered = true;
-    }, API_TIMEOUT_CONSTRAINT);
-  }
-
-  onRetry() {
-    this.triggerTimeoutCheck();
-    this.requestRetry.emit(true);
-  }
 }
