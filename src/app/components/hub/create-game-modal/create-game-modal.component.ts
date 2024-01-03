bcb90e011bda8a3af2fc645c6074a99c082745c9
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-01-02 17:17:14 -0700

    create-game-modal component documentation

--- src/app/components/hub/create-game-modal/create-game-modal.component.ts
+++ src/app/components/hub/create-game-modal/create-game-modal.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { DEFAULT_DENOMINATION_COUNT, DEFAULT_DENOMINATIONS, DEFAULT_MAX_BUY_IN, DEFAULT_MIN_BUY_IN } from '@constants';
 import { ModalController } from '@ionic/angular';
+
+import { DEFAULT_DENOMINATION_COUNT, DEFAULT_DENOMINATIONS, DEFAULT_MAX_BUY_IN, DEFAULT_MIN_BUY_IN } from '@constants';
 import { PoolService, PoolSettings } from 'src/app/services/pool/pool.service';
 
 @Component({
@@ -12,14 +13,12 @@ import { PoolService, PoolSettings } from 'src/app/services/pool/pool.service';
 })
 export class CreateGameModalComponent {
   public poolNameFormControl = new FormControl('', [Validators.required]);
-
   public poolNameFormGroup: FormGroup = this._formBuilder.group({
     poolName: this.poolNameFormControl,
   });
 
   public minBuyInFormControl = new FormControl(DEFAULT_MIN_BUY_IN, [Validators.required]);
   public maxBuyInFormControl = new FormControl(DEFAULT_MAX_BUY_IN, [Validators.required]);
-
   public buyInFormGroup: FormGroup = this._formBuilder.group({
     minBuyInFormControl: this.minBuyInFormControl,
     maxBuyInFormControl: this.maxBuyInFormControl
@@ -42,27 +41,34 @@ export class CreateGameModalComponent {
     private router: Router,
     private _formBuilder: FormBuilder
   ) {
-    
+    this.subscribeToMinMaxBuyInChanges();
+  }
+
+  /**
+   * Subscribes to changes in the min and max buy-in values, cross-checking them against each-other as well
+   */
+  subscribeToMinMaxBuyInChanges() {
     this.minBuyInFormControl.valueChanges.subscribe((value) => {
+      value = Number(value);
       this.validateMinBuyIn(value);
       this.validateMaxBuyIn(this.maxBuyInFormControl.value);
       this.poolSettings.min_buy_in = value ? value : DEFAULT_MIN_BUY_IN;
     });
-
     this.maxBuyInFormControl.valueChanges.subscribe((value) => {
+      value = Number(value);
       this.validateMaxBuyIn(value);
       this.validateMinBuyIn(this.minBuyInFormControl.value);
       this.poolSettings.max_buy_in = value ? value : DEFAULT_MAX_BUY_IN;
     });
-
-    this.poolSettings = {
-      min_buy_in: DEFAULT_MIN_BUY_IN,
-      max_buy_in: DEFAULT_MAX_BUY_IN,
-      denominations: DEFAULT_DENOMINATIONS.slice(0, DEFAULT_DENOMINATION_COUNT)
-    }
   }
 
+  /**
+   * Validates the minimum buy-in value
+   * 
+   * @param {number|null} value The value to validate
+   */
   validateMinBuyIn(value: number|null) {
+    value = Number(value);
     if (!value) {
       return;
     }
@@ -73,7 +79,13 @@ export class CreateGameModalComponent {
     }
   }
 
+  /**
+   * Validates the maximum buy-in value
+   * 
+   * @param {number|null} value The value to validate
+   */
   validateMaxBuyIn(value: number|null) {
+    value = Number(value);
     if (!value) {
       return;
     }
@@ -84,6 +96,9 @@ export class CreateGameModalComponent {
     }
   }
 
+  /**
+   * Creates a game based on the combined form values, navigating to the new game if successful
+   */
   createGame() {
     if (!this.mainFormGroup.valid) {
       return;
@@ -99,30 +114,45 @@ export class CreateGameModalComponent {
     });
   }
 
+  /**
+   * Closes the modal
+   */
   cancel() {
     this.modalCtrl.dismiss(null);
   }
 
-  trackByFn(index: any, item: any) {
-    return index;  
-  }
-
+  /**
+   * Ensures that validation messages appear as soon as user input is detected
+   * 
+   * @param {FormControl} control The formControl assocaited with the input
+   */
   onFocus = (control: FormControl) => control.markAsTouched();
 
+  /**
+   * Handles when the minimum buy-in input loses focus
+   */
   onMinBuyInFocusOut() {
     if (!this.minBuyInFormControl.value) this.minBuyInFormControl.setValue(DEFAULT_MIN_BUY_IN);
   }
 
+  /**
+   * Handles when the maximum buy-in input loses focus
+   */
   onMaxBuyInFocusOut() {
     if (!this.maxBuyInFormControl.value) this.maxBuyInFormControl.setValue(DEFAULT_MAX_BUY_IN);
   }
 
+  /**
+   * A filter that ensures the user can only provide positive integers (no decimals)
+   * 
+   * @param event The event containing the input to filter
+   */
   decimalFilter(event: any) {
-    const reg = /^-?\d*(\.\d{0,1})?$/;
+    const reg = /^\d*$/;
     let input = event.target.value + String.fromCharCode(event.charCode);
- 
+
     if (!reg.test(input)) {
-        event.preventDefault();
+      event.preventDefault();
     }
   }
 }
