065d624a5ea3f84b67782ad3ac5b2882aaa5062b
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-01-02 17:28:01 -0700

    user-pools-table component documentation

--- src/app/components/hub/games-list/user-pools-table/user-pools-table.component.ts
+++ src/app/components/hub/games-list/user-pools-table/user-pools-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { InfiniteScrollCustomEvent } from '@ionic/angular';
+
 import { PoolData } from 'src/app/services/pool/pool.service';
 
 @Component({
@@ -9,30 +10,47 @@ import { PoolData } from 'src/app/services/pool/pool.service';
   styleUrls: ['./user-pools-table.component.scss']
 })
 export class UserPoolsTableComponent implements OnInit {
-  @Input() noNewData: boolean = false;
-  @Input() disabled: boolean = false;
-  @Input() history: boolean = false;
   @Input() dataSource: PoolData[] = [];
+  @Input() disabled: boolean = false;
+  @Input() noNewData: boolean = false;
   @Output() getMoreData: EventEmitter<InfiniteScrollCustomEvent> = new EventEmitter<InfiniteScrollCustomEvent>();
-  public displayedColumns: string[] = [];
 
+  public displayedColumns: string[] = [];
   public unfilteredData: PoolData[] = [];
   private filteredData: PoolData[] = [];
 
   constructor(private router: Router) {}
 
+  /**
+   * Initializes the unfiltered data to be the provided data source
+   */
   ngOnInit() {
     this.unfilteredData = this.dataSource;
   }
 
+  /**
+   * Navigates the user to the selected game
+   * 
+   * @param {PoolData} poolData The pool data associated with the selected game
+   */
   openGame(poolData: PoolData) {
     this.router.navigate(['/', `pool`, poolData.id]);
   }
 
+  /**
+   * Handles infinite scroll events
+   * 
+   * @param {any} event The infinite scroll event
+   */
   onInfiniteScroll(event: any) {
     this.getMoreData.emit(event as InfiniteScrollCustomEvent);
   }
 
+  /**
+   * Handles search events
+   * 
+   * @param {any} event The input event, containing the search value
+   */
   handleSearch(event: any) {
     const searchValue = event.target.value.toLowerCase();
     this.dataSource = this.unfilteredData;
